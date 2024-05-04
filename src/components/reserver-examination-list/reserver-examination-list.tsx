bd67ffc8a8bee22b14a6812b2ec44137b4a8d300
Lucia Mandová <lucka.mandova@gmail.com>
2024-05-04 15:48:40 +0200

    Support delete

--- src/components/reserver-examination-list/reserver-examination-list.tsx
+++ src/components/reserver-examination-list/reserver-examination-list.tsx
@@ -9,67 +9,87 @@ import { RouterPage } from '../../types';
   shadow: true,
 })
 export class ReserverExaminationList {
+  // Events
+  @Event() navigate: EventEmitter<string>;
 
- // Events
- @Event() navigate: EventEmitter<string>;
+  // Properties
+  @Prop() apiBase: string;
 
- // Properties
- @Prop() apiBase: string;
+  // State
+  @State() errorMessages: string[] = [];
 
- // State
- @State() errorMessages: string[] = [];
+  examinations: Examination[];
 
- examinations: Examination[];
+  private async getExaminationsAsync() {
+    try {
+      const response = await ExaminationReservationApiFactory(undefined, this.apiBase).getExaminations();
+      if (response.status < 299) {
+        return response.data;
+      } else {
+        this.errorMessages.push(`Cannot retrieve list of examinations: ${response.statusText}`);
+      }
+    } catch (err: any) {
+      this.errorMessages.push(`Cannot retrieve list of examinations: ${err.message || 'unknown'}`);
+    }
+    return [];
+  }
 
- private async getExaminationsAsync() {
-   try {
-     const response = await ExaminationReservationApiFactory(undefined, this.apiBase).getExaminations();
-     if (response.status < 299) {
-       return response.data;
-     } else {
-       this.errorMessages.push(`Cannot retrieve list of examinations: ${response.statusText}`);
-     }
-   } catch (err: any) {
-     this.errorMessages.push(`Cannot retrieve list of examinations: ${err.message || 'unknown'}`);
-   }
-   return [];
- }
+  async componentWillLoad() {
+    this.examinations = await this.getExaminationsAsync();
+  }
 
- async componentWillLoad() {
-   this.examinations = await this.getExaminationsAsync();
- }
+  private editExamination(id: string) {
+    this.navigate.emit(`examination/${id}`);
+  }
 
- private editExamination(id: string) {
-   this.navigate.emit(`examination/${id}`);
- }
+  private async deleteExamination(id: string) {
+    try {
+      const response = await ExaminationReservationApiFactory(undefined, this.apiBase).deleteExamination(id);
+      if (response.status < 299) {
+        this.examinations = this.examinations.filter(e => e.id !== id);
+      } else {
+        this.errorMessages.push(`Cannot delete examination: ${response.statusText}`);
+      }
+    } catch (err: any) {
+      this.errorMessages.push(`Cannot delete examination: ${err.message || 'unknown'}`);
+    }
+  }
 
- displayExaminations(): JSX.Element[] {
-   return this.examinations.map(examination => (
-     <md-list-item>
-       <md-icon slot="start">home_health</md-icon>
-       <div slot="headline">Examination {examination.room}</div>
-       <md-icon style={{ cursor: 'pointer' }} slot="end" onClick={() => this.editExamination(examination.id)}>
-         edit
-       </md-icon>
-     </md-list-item>
-   ));
- }
+  private getFormattedDate(date: string): string {
+    return new Date(date).toLocaleDateString();
+  }
 
- render() {
-   return (
-     <Host>
-       <div part="header">
-         <h2>Examination list</h2>
-         <button id="navigation-button" onClick={() => this.navigate.emit(RouterPage.HOME)}>
-           Back to home
-         </button>
-       </div>
-       {this.errorMessages.length > 0 ? this.errorMessages.map(e => <div part="error-message">{e}</div>) : <md-list>{this.displayExaminations()}</md-list>}
-       <button id="new-examination-button" onClick={() => this.navigate.emit('examination/@new')}>
-         New examination
-       </button>
-     </Host>
-   );
- }
+  displayExaminations(): JSX.Element[] {
+    return this.examinations.map(examination => (
+      <md-list-item>
+        <md-icon slot="start">home_health</md-icon>
+        <div slot="headline">
+          Patient: {examination.patient} ({this.getFormattedDate(examination.datetime)})
+        </div>
+        <md-icon style={{ cursor: 'pointer' }} slot="end" onClick={() => this.editExamination(examination.id)}>
+          edit
+        </md-icon>
+        <md-icon style={{ cursor: 'pointer' }} slot="end" onClick={() => this.deleteExamination(examination.id)}>
+          delete
+        </md-icon>
+      </md-list-item>
+    ));
+  }
 
+  render() {
+    return (
+      <Host>
+        <div part="header">
+          <h2>Examination list</h2>
+          <button id="navigation-button" onClick={() => this.navigate.emit(RouterPage.HOME)}>
+            Back to home
+          </button>
+        </div>
+        {this.errorMessages.length > 0 ? this.errorMessages.map(e => <div part="error-message">{e}</div>) : <md-list>{this.displayExaminations()}</md-list>}
+        <button id="new-examination-button" onClick={() => this.navigate.emit('examination/@new')}>
+          New examination
+        </button>
+      </Host>
+    );
+  }
 }
--- src/components/reserver-room-list/reserver-room-list.tsx
+++ src/components/reserver-room-list/reserver-room-list.tsx
@@ -1,6 +1,6 @@
 import { Component, Event, EventEmitter, Host, Prop, State, h } from '@stencil/core';
 import type { JSX } from '@stencil/core/internal';
-import { RoomReservation, RoomReservationApiFactory } from '../../api/reserver';
+import { DepartmentsApiFactory, Doctor, Room, RoomReservation, RoomReservationApiFactory } from '../../api/reserver';
 import { RouterPage } from '../../types';
 
 @Component({
@@ -18,7 +18,9 @@ export class ReserverRoomList {
   // State
   @State() errorMessages: string[] = [];
 
-  reservations: RoomReservation[];
+  reservations: RoomReservation[] = [];
+  rooms: Room[] = []
+  doctors: Doctor[] = [];
 
   private async getReservationsAsync() {
     try {
@@ -34,21 +36,77 @@ export class ReserverRoomList {
     return [];
   }
 
+  private async getRoomsAsync() {
+    try {
+      const response = await RoomReservationApiFactory(undefined, this.apiBase).getRooms();
+      if (response.status < 299) {
+        return response.data;
+      } else {
+        this.errorMessages.push(`Cannot retrieve list of rooms: ${response.statusText}`);
+      }
+    } catch (err: any) {
+      this.errorMessages.push(`Cannot retrieve list of rooms: ${err.message || 'unknown'}`);
+    }
+    return [];
+  }
+
+  private async getDoctorsAsync() {
+    try {
+      const response = await DepartmentsApiFactory(undefined, this.apiBase).getDoctors();
+      if (response.status < 299) {
+        return response.data;
+      } else {
+        this.errorMessages.push(`Cannot retrieve list of doctors: ${response.statusText}`);
+      }
+    } catch (err: any) {
+      this.errorMessages.push(`Cannot retrieve list of doctors: ${err.message || 'unknown'}`);
+    }
+    return [];
+  }
+
   async componentWillLoad() {
     this.reservations = await this.getReservationsAsync();
+    this.rooms = await this.getRoomsAsync();
+    this.doctors = await this.getDoctorsAsync();
   }
 
   private editReservation(id: string) {
     this.navigate.emit(`reservation/${id}`);
   }
 
+  private async deleteReservation(id: string) {
+    try {
+      const response = await RoomReservationApiFactory(undefined, this.apiBase).deleteReservation(id);
+      if (response.status < 299) {
+        this.reservations = this.reservations.filter(e => e.id !== id);
+      } else {
+        this.errorMessages.push(`Cannot delete reservation: ${response.statusText}`);
+      }
+    } catch (err: any) {
+      this.errorMessages.push(`Cannot delete reservation: ${err.message || 'unknown'}`);
+    }
+  }
+
+  private getRoomName(roomId: string): string {
+    const room = this.rooms.find(r => r.id === roomId);
+    return room ? room.roomNumber : 'unknown';
+  }
+
+  private getDoctorName(doctorId: string): string {
+    const doctor = this.doctors.find(d => d.id === doctorId);
+    return doctor ? doctor.name : 'unknown';
+  }
+
   displayReservations(): JSX.Element[] {
     return this.reservations.map(reservation => (
       <md-list-item>
         <md-icon slot="start">home_health</md-icon>
-        <div slot="headline">Room {reservation.room}</div>
+        <div slot="headline">Room {this.getRoomName(reservation.room)} ({this.getDoctorName(reservation.doctor)})</div>
         <md-icon style={{ cursor: 'pointer' }} slot="end" onClick={() => this.editReservation(reservation.id)}>
           edit
+        </md-icon>
+        <md-icon style={{ cursor: 'pointer' }} slot="end" onClick={() => this.deleteReservation(reservation.id)}>
+          delete
         </md-icon>
       </md-list-item>
     ));
